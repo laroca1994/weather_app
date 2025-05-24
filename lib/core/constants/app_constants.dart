0d088988a337d413efea463e389e76a7ff7e5d04
Jang <jacunaferrer@gmail.com>
2025-05-24 20:19:35 -0300

    style: standardize string quotes in defaultCities list

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,10 +2,10 @@ class AppConstants {
   static const String openWeatherMapBaseUrl =
       'https://api.openweathermap.org/data/2.5';
   static const List<(String, String)> defaultCities = [
-    ("Montevideo", 'UY'),
-    ("Londres", 'GB'), // London
-    ("San Pablo", 'BR'), // Sao Paulo
-    ("Buenos Aires", 'AR'),
-    ("Munich", 'DE'),
+    ('Montevideo', 'UY'),
+    ('Londres', 'GB'), // London
+    ('San Pablo', 'BR'), // Sao Paulo
+    ('Buenos Aires', 'AR'),
+    ('Munich', 'DE'),
   ];
 }
